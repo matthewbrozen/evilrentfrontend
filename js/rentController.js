4d37b17b683f19e9defd3ba12e07ecedf435ebcb
matthew brozen <matthewbrozen@gmail.com>
2016-11-29 13:27:40 -0800

    initial commit

---
+++ js/rentController.js
@@ -0,0 +1,23 @@
+angular.module('EvilrentApp')
+    .controller('RentController', RentController);
+
+//injecting http
+RentController.$inject = ['$http'];
+// main function
+function RentController($http) {
+  var self = this;
+  self.newRent={};
+  self.addRent= addRent;
+
+
+
+  //post report method need to store _id from response
+  function addRent(){
+   $http
+     .post('http://localhost:3000/rents', self.newRent)
+     .success(function(response){
+       console.log(response);
+   });
+  }
+
+}
