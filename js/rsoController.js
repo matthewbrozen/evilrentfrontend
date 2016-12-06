b69ecec9d963d34dfbaa0464605779154b3b1409
matthew brozen <matthewbrozen@gmail.com>
2016-12-06 10:25:26 -0800

    whoops

--- js/rsoController.js
+++ js/rsoController.js
@@ -19,10 +19,10 @@ function RsoController($http) {
        console.log(response);
        if(response=="RSO"){
        var rso = response;
-       document.getElementById('rso').innerHTML("It looks like your property is rent controlled, follow the prompts and we will let you know more!");
+       document.getElementById('rso').innerHTML="It looks like your property is rent controlled, follow the prompts and we will let you know more!";
      }
      else{
-       document.getElementById('rso').innerHTML("We're not sure if you're propety is rent controlled, follow the prompts and we will get back to you after more research!");
+       document.getElementById('rso').innerHTML="We're not sure if you're propety is rent controlled, follow the prompts and we will get back to you after more research!";
      }
    });
   }
